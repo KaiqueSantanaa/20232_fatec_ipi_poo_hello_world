464bb7e5bcf89708d40d8f6316acf7c1ce1e8309
Kaique Santana <sleeptube0@gmail.com>
2023-08-31 16:02:54 -0300

    ap01(java): Hello World
    - usando JOptionPane

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,11 @@
+//pacote padrão: java.lang
+//importado automaticamente
+import javax.swing.JOptionPane;
 public class HelloWorld{
     public static void main (String [] args){
         //stdin stdout
-        System.out.println("Hello, World");
+        JOptionPane.showMessageDialog(null, "Hello, World");
+        //System.out.println("Hello, World");
     }
 }
 //O compilador produz bytecode, é a linguagem que a JVM entende, JVM interpreta bytecode
